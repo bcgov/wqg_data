32635eff7b59b20173875e842b72e1beded17633
joethorley <joe@poissonconsulting.ca>
2020-01-30 11:45:17 -0800

    output missing codes

--- readwritecsv.R
+++ readwritecsv.R
@@ -1,8 +1,22 @@
 library(tidyverse)
 library(magrittr)
+library(wqbc)
+
+codes <- wqbc::ems_codes
+codes$EMS_CODE %<>% paste0("EMS_", .)
 
 data <- read_csv("all_wqgs.csv")
 
-data$EMS_Code %<>% paste0("EMS_", .)
+#write_csv(data, "all_wqgs.csv", na = "")
 
-write_csv(data, "all_wqgs.csv", na = "")
+
+missing_codes <- anti_join(data, codes, by = c(EMS_Code = "EMS_CODE"))
+
+missing_codes %<>% distinct(Variable, EMS_Code)
+
+missing_codes$EMS_Code[missing_codes$EMS_Code == "EMS_NA"] <- NA
+
+filter(missing_codes, is.na(EMS_Code))
+
+filter(missing_codes, !is.na(EMS_Code))
+
