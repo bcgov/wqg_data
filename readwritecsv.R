a4e0012a7c74749c628aca29b3b209b0056e1f46
joethorley <joe@poissonconsulting.ca>
2020-01-30 09:47:15 -0800

    separate by ,

--- readwritecsv.R
+++ readwritecsv.R
@@ -3,5 +3,19 @@ library(magrittr)
 
 data <- read_csv("all_wqgs.csv")
 
-write_csv(data, "all_wqgs.csv", na = "")
+missing <- filter(data, is.na(EMS_Code))
 
+data %<>% filter(!is.na(EMS_Code))
+
+data %<>% separate(EMS_Code, paste0("EMS_Code", 1:4), sep = "\\s*,\\s*")
+
+data %<>% pivot_longer(paste0("EMS_Code", 1:4), names_to = "EMS_Code_Number", values_to = "EMS_Code")
+
+data %<>%
+  filter(!is.na(EMS_Code))
+
+data %<>% select(colnames(missing))
+
+data %<>% bind_rows(missing)
+
+write_csv(data, "all_wqgs.csv", na = "")
