46d6d4edd215cb13a6e260492d566adc1e636488
joethorley <joe@poissonconsulting.ca>
2020-01-30 12:42:28 -0800

    get codes from rems

--- readwritecsv.R
+++ readwritecsv.R
@@ -1,14 +1,21 @@
 library(tidyverse)
 library(magrittr)
 library(wqbc)
-
-codes <- wqbc::ems_codes
-codes$EMS_CODE %<>% paste0("EMS_", .)
+library(rems)
 
 data <- read_csv("all_wqgs.csv")
 
 #write_csv(data, "all_wqgs.csv", na = "")
 
+#codes <- wqbc::ems_codes
+
+codes <- rems::ems_parameters %>%
+        select(EMS_CODE = PARAMETER_CODE,
+               EMS_VARIABLE = PARAMETER) %>%
+        mutate_all(stringr::str_trim, side = "both") %>%
+        distinct()
+
+codes$EMS_CODE %<>% paste0("EMS_", .)
 
 missing_codes <- anti_join(data, codes, by = c(EMS_Code = "EMS_CODE"))
 
