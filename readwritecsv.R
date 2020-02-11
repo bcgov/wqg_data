aa4b06dcc7509861cd2ba0435054e943e640da4a
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-11 14:08:32 -0800

    capitalize Colour true

--- readwritecsv.R
+++ readwritecsv.R
@@ -9,7 +9,7 @@ data <- read_csv("all_wqgs.csv")
 data_old <- data
 
 data$Variable %<>%
-  str_replace_all("lodo-2-propynl butylcarbamate", "Lodo-2-propynl butylcarbamate")
+  str_replace_all("Colour true", "Colour True")
 
 if(FALSE) {
   patch <- diff_data(data_old, data)
