a2005eb5a7dfae6d95dd3467c72915eed09ec0f3
joethorley <joe@poissonconsulting.ca>
2020-02-11 13:57:46 -0800

    total to Total

--- readwritecsv.R
+++ readwritecsv.R
@@ -9,7 +9,7 @@ data <- read_csv("all_wqgs.csv")
 data_old <- data
 
 data$Variable %<>%
-  str_replace("^Dissolved Oxygen$", "Oxygen Dissolved")
+  str_replace_all("total", "Total")
 
 if(FALSE) {
   patch <- diff_data(data_old, data)
