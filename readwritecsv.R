f210d70f15ac89a4bb97bdf1797fcc15aa08d22f
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-11 14:02:54 -0800

    capitalize hydroxyphenol

--- readwritecsv.R
+++ readwritecsv.R
@@ -9,7 +9,7 @@ data <- read_csv("all_wqgs.csv")
 data_old <- data
 
 data$Variable %<>%
-  str_replace_all("total", "Total")
+  str_replace_all("hydroxyphenol", "Hydroxyphenol")
 
 if(FALSE) {
   patch <- diff_data(data_old, data)
