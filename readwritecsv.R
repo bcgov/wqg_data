79e20b6de984a5b0e8d30fa3f49d58a72756411c
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-13 15:08:35 -0800

    fix Condition R expr

--- readwritecsv.R
+++ readwritecsv.R
@@ -11,8 +11,8 @@ data_old <- data
 setdiff(unique(data$Condition), NA) %>% sort
 
 data$Condition %<>%
-  str_replace_all("^EMS_0107 \\| EMS_1107 <= 10$",
-                  "EMS_0107 <= 10 | EMS_1107 <= 10")
+  str_replace_all("^EMS_0107 \\| EMS_1107 <= 100$",
+                  "EMS_0107 <= 100 | EMS_1107 <= 100")
 
 if(FALSE) {
   patch <- diff_data(data_old, data)
